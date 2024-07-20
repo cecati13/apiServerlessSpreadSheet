58fcc0ec79c4e443e99c76637e0b35d353e3aa26
Damian <dvnegrete@gmail.com>
2024-07-19 19:33:21 -0600

    Añadiendo CORS

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import { http } from "@google-cloud/functions-framework";
 import express from "express";
 import { routerAPI } from "./src/router.js";
-
-// Register an HTTP function with the Functions Framework that will be executed
-// when you make an HTTP request to the deployed function's endpoint.
+import { cors } from "./src/middleware/cors.js";
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 routerAPI(app);
 
---
+++ src/middleware/cors.js
@@ -0,0 +1,20 @@
+const allowedOrigins = [
+  "https://cecati13.edu.mx",
+  //dev
+  "http://localhost:5501",
+  undefined,
+];
+
+export function cors(req, res, next) {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Allow", "GET");
+    next();
+  }
+}
